9fd349dc441b2f0f5baefbcc60f966a014d61cd8
santanu.jana@aol.com <santanu.jana@aol.com>
2026-04-07 12:19:03 +0530

    feat: add metadata configuration for invoices and root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import "./ui/global.css";
-import { inter } from '@/app/ui/font';
+import { inter } from "@/app/ui/font";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard", // The %s in the template will be replaced with the specific page title.
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Learn Dashboard built with App Router.",
+  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+};
 
 export default function RootLayout({children}: {children: React.ReactNode;}) {
   return (
